57b6ee64f8d38d85676d9ceca36f66703db528d1
Ethan Quinlan <ethan.quinlan2016@gmail.com>
2022-12-20 22:56:30 -0500

    Added addOption for more employees and then responses for Intern and Engineer

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-const teamMembers = [];
+// Variable containing user input
+const userEmployee = [];
 
 function newMember() {
     inquirer.prompt([
@@ -60,9 +61,100 @@ function newMember() {
         },
     ])
 
-    
+
+    .then(answers => {
+
+        if (answers.role === 'Manager') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'office',
+                    message:'Enter office number:',
+                    validate: officeInput => {
+                        if (officeInput) {
+                            return true;
+                        } else {
+                            return 'Please enter office No.';
+                        }
+                    }
+                    
+                }
+            ])
+            .then(response => {
+                console.log(response.office);
+                const ManagerTeam = new Manager (answers.name, answers.email, answers.id, answers.role, response.office)
+                userEmployee.push(ManagerTeam);
+                addOption()
+            })
+        }else if(answers.role === 'Engineer' ){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'gitHub',
+                    message:'Enter Github name:',
+                    validate: githubInput => {
+                        if (githubInput) {
+                            return true;
+                        } else {
+                            return 'Please enter gitHub username.';
+                        }
+                    }
+                }
+            ])
+            .then(response => {
+                console.log(response.gitHub);
+                const EngineerTeam = new Engineer (answers.name, answers.email, answers.id, answers.role, response.gitHub)
+                userEmployee.push(EngineerTeam);
+                addOption()
+            })
+        } else if (answers.role === 'Intern'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'school',
+                    message:'Enter School name:', 
+                    validate: function (schoolInput) {
+                        if (schoolInput) {
+                            return true;
+                        } else {
+                            return 'Please enter school  name.';
+                        }
+                    }
+
+                }
+            ])
+            .then(response =>{
+                console.log(response.school);
+                const internTeam = new Intern (answers.name,  answers.email, answers.id, answers.role, response.school)
+                userEmployee.push(internTeam);
+                addOption()
+            })
+        }
+
+        else {
+            const employeeTeam = new Employee (answers.name, answers.email, answers.id, answers.role);
+            userEmployee.push(employeeTeam);
+            addOption()
+
+        }
+
+        //add option 
+
+        function addOption() {
+            inquirer.prompt([
+                {
+                    type:'confirm',
+                    name: 'addMore',
+                    message:'Would you like to add another Employee?'
+                }
+            ])
+          
+        }
+    })
+
 }
+// function to write file
 
-
+newMember()
 
 
